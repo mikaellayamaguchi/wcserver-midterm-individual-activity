afc12f68731afb9d9a7537c911a5003d873c6bd8
mikaellayamaguchi <mikaellayamaguchi23@gmail.com>
2025-09-01 20:47:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const app = express();
 
-// Root
 app.get('/', (req, res) => {
   res.send('My New App!');
 });
 
-// Heroes list
 app.get('/api/heroes', (req, res) => {
   res.send(['Superman', 'Iron Man', 'Batman', 'Hulk']);
 });
 
-// Single param
 app.get('/api/heroes/:id', (req, res) => {
   res.send(req.params.id);
 });
 
-// --- OLD duplicate route (commented out) ---
 // app.get('/api/heroes/:title/:publisher', (req, res) => {
 //   res.send(req.params);
 // });
 
-// Multi params + query params (working one)
 app.get('/api/heroes/:title/:publisher', (req, res) => {
   res.send({
     params: req.params,
@@ -29,5 +24,4 @@ app.get('/api/heroes/:title/:publisher', (req, res) => {
   });
 });
 
-// Start server
 app.listen(3000, () => console.log('Listening on port 3000'));
